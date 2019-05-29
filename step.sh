13f32ffac342a526138c8fe9fc370e77b919d55f
Herman Olsson <herman.olsson@viaplay.com>
2019-05-29 14:54:02 +0200

    Add TLS auth support to openvpn command on Mac OS

--- step.sh
+++ step.sh
@@ -42,8 +42,12 @@ EOF
     echo ${ca_crt} | base64 -D -o ca.crt > /dev/null 2>&1
     echo ${client_crt} | base64 -D -o client.crt > /dev/null 2>&1
     echo ${client_key} | base64 -D -o client.key > /dev/null 2>&1
+    echo ${user_pass} | base64 -D -o user-pass > /dev/null 2>&1
+    echo ${tls_auth} | base64 -D -o tls-auth.key > /dev/null 2>&1
 
     sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key > /dev/null 2>&1 &
+
+    sudo openvpn --client --dev tun --proto ${proto} --remote ${host} ${port} --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key --ns-cert-type server --auth-user-pass user-pass --tls-auth tls-auth.key 1
 
     sleep 5
 
